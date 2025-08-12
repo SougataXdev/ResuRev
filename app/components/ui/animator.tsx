64227405fa04498d0d1d2c9480d5ec95d29bf607
SougataXdev <162133497+SougataXdev@users.noreply.github.com>
2025-08-12 21:10:09 +0530

    Update animator.tsx

--- app/components/ui/animator.tsx
+++ app/components/ui/animator.tsx
@@ -6,7 +6,7 @@ export default function AnimatedSection({ children }: React.PropsWithChildren<{}
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.5 }} 
+      viewport={{ once: true, amount: 0.2 }} 
       // amount: 0.5 means animation starts when 50% of the section is visible
       transition={{ delay, duration: 0.8, ease: "easeIn" }}
     >
